c0da64d30b4c803447c8a7be7e0354aae5139005
Methodog <sittingbottom@gmail.com>
2011-11-15 21:45:57 +0000

    add test ajax to search autocomplete

---
+++ test_ajax/autocomplete/articles.php
@@ -0,0 +1,4 @@
+<ul>
+<li><a href="#">article <?php echo $_GET['q']; ?> example</a></li>
+<li><a href="#">example <?php echo $_GET['q']; ?> article</a></li>
+</ul>
---
+++ test_ajax/autocomplete/keyterms.php
@@ -0,0 +1,4 @@
+<ul>
+<li><a href="#">key term <?php echo $_GET['q']; ?> example</a></li>
+<li><a href="#">example <?php echo $_GET['q']; ?> key term</a></li>
+</ul>
